f2d469d855cfd316c94699ad5eb3bd9b29b61147
EzYouBy1 <erwan05.yt@gmail.com>
2024-11-20 17:51:45 +0100

    modif profil

--- profil.php
+++ profil.php
@@ -1,3 +1,12 @@
+<?php
+session_start(); // Démarrer la session
+
+$userId = isset($_SESSION['user_id']) ? $_SESSION['user_id'] : null;
+
+// Vérifie si l'utilisateur est connecté et admin
+$is_admin = isset($_SESSION['is_admin']) && $_SESSION['is_admin'] === true;
+?>
+
 <!DOCTYPE html>
 <html lang="fr">
 <head>
@@ -7,6 +16,58 @@
   <link rel="stylesheet" href="stylecss/styleProf.css">
 </head>
 <body>
+<header>
+        <div class="header-container">
+            <!-- Logo -->
+            <div class="logo">
+                <img src="image/logoAdiil.png" alt="Logo BDE">
+            </div>
+
+            <!-- Menu Admin -->
+            <?php if ($is_admin): ?>
+            <div class="dropdown">
+                <button class="dropdown-toggle">Admin</button>
+                <div class="dropdown-menu">
+                <a href="#">Espace partagé</a>
+                <a href="#">Gestion membre</a>
+                <a href="#">Statistique</a>
+                <a href="#">Banque</a>
+                <a href="#">Gestion site</a>
+                </div>
+            </div>
+            <?php endif; ?>
+
+            <!-- Navigation -->
+            <nav>
+                <ul class="nav-links">
+                    <li><a href="accueil.php" class="active">Accueil</a></li>
+                    <li><a href="evenements.php">Événements</a></li>
+                    <li><a href="boutique.php">Boutique</a></li>
+                    <li><a href="bde.php">BDE</a></li>
+                    <li><a href="faq.php">FAQ</a></li>
+                </ul>
+            </nav>
+
+            <!-- Boutons / Profil -->
+            <div class="header-buttons">
+                <?php
+                if ($userId!=null):
+                    // Utilisateur connecté
+                    $profileImage = !empty($_SESSION['Photo_user']) ? $_SESSION['Photo_user'] : 'image/ppBaptProf.jpg';
+                ?>
+                    <img src="<?= htmlspecialchars($profileImage) ?>" alt="Profil" class="profile-icon">
+                    <form action="logout.php" method="post" class="logout-form">
+                        <button type="submit" class="logout-button">Se déconnecter</button>
+                    </form>
+                    <img src="image/logoPanier.png" alt="Panier" class="cartIcon">
+                <?php else: ?>
+                    <!-- Boutons si non connecté -->
+                    <a href="connexion.html" class="connectButtonHeader">Se connecter</a>
+                    <a href="inscription.html" class="registerButtonHeader">S'inscrire</a>
+                <?php endif; ?>
+            </div>
+        </div>
+    </header>
   <?php
     // Variables dynamiques pour le profil
     $nom = "Erwan";
