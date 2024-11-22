ae024957b90c959430de7bff64c48e6541410f61
EvannC <150244159+EvannC@users.noreply.github.com>
2024-11-22 10:18:18 +0000

    jsp tjr pas

--- boutique.php
+++ boutique.php
@@ -237,6 +237,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST' && $is_admin) {
                 } catch (PDOException $e) {
                     echo "<p style='color:red;'>Erreur : " . $e->getMessage() . "</p>";
                 }
+                gros caca prout de ta grosse daronne la reine des chauves
                 ?>
             </div>
         </div>
