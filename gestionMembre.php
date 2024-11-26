b0af6c646190488d86673b20c224903f453d3b26
SenseiChan <ysopetom@gmail.com>
2024-11-26 15:03:43 +0100

    Tom ajout header gestMembre

--- gestionMembre.php
+++ gestionMembre.php
@@ -68,51 +68,7 @@ foreach ($membres as $membre) {
     <link rel="stylesheet" href="stylecss/styleGestionMembre.css"> <!-- Lien vers le fichier CSS -->
 </head>
 <body>
-    <header>
-        <div class="header-container">
-            <!-- Logo -->
-            <div class="logo">
-                <img src="image/logoAdiil.png" alt="Logo BDE">
-            </div>
-
-            <div class="dropdown">
-                <button class="dropdown-toggle">Admin</button>
-                <div class="dropdown-menu">
-                    <a href="#">Espace partagé</a>
-                    <a href="#">Gestion membre</a>
-                    <a href="#">Statistique</a>
-                    <a href="#">Banque</a>
-                    <a href="#">Gestion site</a>
-                </div>
-            </div>
-
-            <nav>
-                <ul class="nav-links">
-                    <li><a href="accueil.php">Accueil</a></li>
-                    <li><a href="evenements.php">Événements</a></li>
-                    <li><a href="boutique.php">Boutique</a></li>
-                    <li><a href="bde.php">BDE</a></li>
-                    <li><a href="faq.php">FAQ</a></li>
-                </ul>
-            </nav>
-
-            <!-- Boutons / Profil -->
-            <div class="header-buttons">
-                <?php
-                if ($userId != null):
-                    // Utilisateur connecté
-                    $profileImage = !empty($_SESSION['Photo_user']) ? $_SESSION['Photo_user'] : 'image/default.png';
-                ?>
-                    <img src="<?= htmlspecialchars($profileImage) ?>" alt="Profil" class="profile-icon">
-                    <form action="logout.php" method="post" class="logout-form">
-                        <button type="submit" class="logout-button">Se déconnecter</button>
-                    </form>
-                    <img src="image/logoPanier.png" alt="Panier" class="cartIcon">
-                <?php endif; ?>
-            </div>
-        </div>
-    </header>
-
+<?php include 'header.php'; ?>
     <main>
         <br><br><br>
         <div class="admin-section">
